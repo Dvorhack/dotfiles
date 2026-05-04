fe878e9db455d0dfe7def3c5e312afdd6881356a
Dvorhack <paul@serviel.fr>
2026-05-04 22:09:19 +0200

    waybar config

--- home/profiles/laptop.nix
+++ home/profiles/laptop.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   imports = [
@@ -15,7 +20,31 @@
     if [ -z "$WAYLAND_DISPLAY" ] && [ -n "$XDG_VTNR" ] && [ "$XDG_VTNR" -eq 1 ] ; then
         exec sway
     fi
-    '';
+  '';
 
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
+    ];
+    config = {
+      sway = {
+        default = [
+          "wlr"
+          "gtk"
+        ];
+        # Specific interfaces that should use gtk
+        "org.freedesktop.impl.portal.Settings" = [ "gtk" ];
+        "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+      };
+      common = {
+        default = [ "gtk" ];
+      };
+    };
+    xdgOpenUsePortal = true;
+  };
+
+  services.flameshot.enable = true;
 
 }
--- modules/home/waybar.nix
+++ modules/home/waybar.nix
@@ -4,45 +4,71 @@
   programs.waybar = {
     enable = true;
 
-    settings = [
-      {
-        mainBar = {
-          layer = "top";
-          position = "top";
-          height = 30;
-          output = [
-            "eDP-1"
-            "HDMI-A-1"
-          ];
-          modules-left = [
-            "sway/workspaces"
-            "sway/mode"
-            "wlr/taskbar"
-          ];
-          modules-center = [
-            "sway/window"
-            "custom/hello-from-waybar"
-          ];
-          modules-right = [
-            "mpd"
-            "custom/mymodule#with-css-id"
-            "temperature"
-          ];
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        height = 30;
+        output = [
+          "eDP-1" # laptop screen
+          "DP-1" # wide screen
+        ];
+        modules-left = [
+          "sway/workspaces"
+          "sway/mode"
+        ];
+        modules-center = [
+          "sway/window"
+          "custom/hello-from-waybar"
+        ];
+        modules-right = [
+          "network"
+          "custom/public-ip"
+          "disk"
+          "battery"
+          "backlight"
+          "clock"
+          "tray"
+        ];
 
-          "sway/workspaces" = {
-            disable-scroll = true;
-            all-outputs = true;
-          };
-          "custom/hello-from-waybar" = {
-            format = "hello {}";
-            max-length = 40;
-            interval = "once";
-            exec = pkgs.writeShellScript "hello-from-waybar" ''
-              echo "from within waybar"
-            '';
-          };
+        "disk" = {
+          "unit" = "GB";
+          "format" = "{used}/{total}";
         };
-      }
-    ];
+
+        "network" = {
+          "format" = "{ifname}: {ipaddr}";
+        };
+
+        "backlight" = {
+          "format" = "Light: {percent}%";
+        };
+
+        "battery" = {
+          "format" = "Bat: {capacity}%";
+          "format-charging" = "Charing: {capacity}% ({time})";
+        };
+
+        "custom/public-ip" = {
+          format = "{}";
+          exec = pkgs.writeShellScript "public-ip" ''
+            curl -4 ifconfig.me
+          '';
+        };
+
+        "sway/workspaces" = {
+          disable-scroll = true;
+          all-outputs = false;
+        };
+        "custom/hello-from-waybar" = {
+          format = "hello {}";
+          max-length = 40;
+          interval = "once";
+          exec = pkgs.writeShellScript "hello-from-waybar" ''
+            echo "from within waybar"
+          '';
+        };
+      };
+    };
   };
 }
